7a62006d7c172985b86f6ab3e733abb948dbf3d4
isikveren <lauxunzi@outlook.com>
2022-08-25 19:50:43 +0800

    考研东北大学 加油！

---
+++ chapter2/2_1_1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+#define Max 50
+struct SqList
+{
+    int a[Max] = {};
+    int length = 0;
+};
+int listdelete(SqList &s, int &ele)
+{
+    if (s.length == 0)
+    {
+        return false;
+    }
+    int min = s.a[0];
+    int flag = 0;
+    for (int i = 1; i < s.length; i++)
+    {
+        if (s.a[i] < min)
+        {
+            min = s.a[i];
+            flag = i;
+        }
+    }
+    s.a[flag] = s.a[s.length - 1];
+    return s.a[flag];
+}
+void printsql(SqList &s)
+{
+
+    for (int i = 0; i < s.length; i++)
+    {
+        cout << s.a[i] << " ";
+    }
+}
+
+int main()
+{
+    SqList s;
+    int x;
+    printsql(s);
+    if (listdelete(s, x))
+    {
+        cout << "最小元素K = " << x << endl;
+    }
+    else
+    {
+        cout << "错误";
+    }
+    printsql(s);
+    return 0;
+}
