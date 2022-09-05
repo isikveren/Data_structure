ef2b4eb8542396e7a29f4bba4e5b277e0b433869
isikveren <lauxunzi@outlook.com>
2022-09-05 10:50:42 +0800

    update

---
+++ chapter2/2_1_2.cpp
@@ -0,0 +1,15 @@
+//逆置顺序表
+#include <iostream>
+using namespace std;
+#define Max 50
+struct SqList{
+    int a[Max] = {1,2,3,4,5,6,7};
+    int length = 7;
+};
+void reverse(SqList &s){
+    
+}
+int main(){
+
+    return 0;
+}
